a1acaa81956a2324a79876805b971859791d4aa3
Jesse Young <jgyoung295@gmail.com>
2018-02-19 18:15:34 -0500

    completed assignment 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  showPara = true;
+
+  btnClicks = [];
+  btnClickNum = 0;
+
+  toggleDisplay() {
+    this.btnClickNum++;
+    this.btnClicks.push(this.btnClickNum);
+    console.table(this.btnClicks);
+    return this.showPara === true ? this.showPara = false : this.showPara = true;
+  }
+
+  getColor() {
+    return 'blue';
+  }
 }

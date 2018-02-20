b898859a9672846479cda099d8d2258122269dda
Jesse Young <jgyoung295@gmail.com>
2018-02-20 00:38:11 -0500

    completed assignment 4

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  oddNums: number[] = [];
+  evenNums: number[] = [];
+
+  onIntervalFired(firedNum: number) {
+    if (firedNum % 2 === 0) {
+      this.evenNums.push(firedNum);
+    } else {
+      this.oddNums.push(firedNum);
+    }
+  }
 }

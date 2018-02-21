2117c20737532a2200b81b7d06aaea2e68216a59
Jesse Young <jgyoung295@gmail.com>
2018-02-21 01:37:30 -0500

    complete services assignment

---
+++ services-assignment-start/src/app/counter.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CounterService {
+
+  activeToInactiveCounter = 0;
+  inactiveToActiveCounter = 0;
+
+  constructor() { }
+
+  incrementActiveToInactive() {
+    this.activeToInactiveCounter++;
+    console.log('Active to Inactive: ' + this.activeToInactiveCounter);
+  }
+
+  incrementInactiveToActive() {
+    this.inactiveToActiveCounter++;
+    console.log('Inactive to Active: ' + this.inactiveToActiveCounter);
+  }
+
+}

701dbd0a4fa218633574b3f304437f4a8307329e
Jesse Young <jgyoung295@gmail.com>
2018-02-22 20:54:16 -0500

    learn passing and retreiving query params and fragments

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -25,6 +25,9 @@ const appRoutes: Routes = [
   },
   {
     path: 'servers', component: ServersComponent
+  },
+  {
+    path: 'servers/:id/edit', component: EditServerComponent
   }
 ];
 
--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -13,9 +13,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer(id: number) {
     // complex calculation or something other
-    this.route.navigate(['servers']);
+    this.route.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 
 }

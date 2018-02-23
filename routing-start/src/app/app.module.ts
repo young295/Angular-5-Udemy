63c9b0ba04e30145dfadca76f6b50c97f06fe145
Jesse Young <jgyoung295@gmail.com>
2018-02-22 20:38:43 -0500

    learn passing parameters in routing, observables with params

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -21,6 +21,9 @@ const appRoutes: Routes = [
     path: 'users', component: UsersComponent
   },
   {
+    path: 'users/:id/:name', component: UserComponent
+  },
+  {
     path: 'servers', component: ServersComponent
   }
 ];

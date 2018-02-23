d836be3ed89dbff53613149e3ce2e6098c6e9daf
Jesse Young <jgyoung295@gmail.com>
2018-02-22 21:47:32 -0500

    implement child routing

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -18,20 +18,23 @@ const appRoutes: Routes = [
     path: '', component: HomeComponent
   },
   {
-    path: 'users', component: UsersComponent
+    path: 'users', component: UsersComponent, children: [
+      {
+        path: ':id/:name', component: UserComponent
+      }
+    ]
   },
   {
-    path: 'users/:id/:name', component: UserComponent
+    path: 'servers', component: ServersComponent, children: [
+      {
+        path: ':id', component: ServerComponent
+      },
+      {
+        path: ':id/edit', component: EditServerComponent
+      }
+    ]
   },
-  {
-    path: 'servers', component: ServersComponent
-  },
-  {
-    path: 'servers/:id', component: ServerComponent
-  },
-  {
-    path: 'servers/:id/edit', component: EditServerComponent
-  }
+
 ];
 
 @NgModule({

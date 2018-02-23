c39dfb5d99f9c4463d1af09bfac51021c8962d7a
Jesse Young <jgyoung295@gmail.com>
2018-02-22 22:58:32 -0500

    add fake login/logout to fake auth service

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -16,6 +17,14 @@ export class HomeComponent implements OnInit {
   onLoadServer(id: number) {
     // complex calculation or something other
     this.route.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
